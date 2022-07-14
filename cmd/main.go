137488648d34f0f2606baabc43b6cc78963029bc
Michael Highstead <highstead@gmail.com>
2022-07-14 09:38:06 -0400

    empty repo

---
+++ cmd/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"flag"
+
+	"github.com/sirupsen/logrus"
+)
+
+var (
+	debug = flag.Bool("debug", false, "Enables debug logging")
+)
+
+func main() {
+	flag.Parse()
+	log := logrus.NewEntry(logrus.StandardLogger())
+	if *debug {
+		logrus.SetLevel(logrus.DebugLevel)
+	}
+
+	log.Infoln("Hello world")
+
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/highstead/failover-stuff
+
+go 1.18
+
+require (
+	github.com/sirupsen/logrus v1.8.1 // indirect
+	golang.org/x/sys v0.0.0-20220712014510-0a85c31ab51e // indirect
+)
